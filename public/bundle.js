269a6b2b760699d6faea990eb9f5300a33a74bdc
zky <623212389@qq.com>
2017-01-20 00:57:44 +0800

    1.letAndConst.js

--- public/bundle.js
+++ public/bundle.js
@@ -49,12 +49,25 @@
 	/**
 	 * Created by zhaoky on 2017/1/19.
 	 */
-	{
-	  var _a = 10;
-	  var b = 20;
-	}
+	// {
+	// 	let c = 10;
+	// 	var b = 20;
+	// }
+	//
+	// console.log(c,b);
 	
-	console.log(a, b);
+	// for(let i=0;i<10;i++){
+	//
+	// }
+	// console.log(i);  //not defined
+	
+	var a = [];
+	for (var i = 0; i < 10; i++) {
+	    a[i] = function () {
+	        console.log(i);
+	    };
+	}
+	a[6](); // 10
 
 /***/ }
 /******/ ]);
